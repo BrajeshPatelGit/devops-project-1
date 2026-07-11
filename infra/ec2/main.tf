634954bfc8543741a45e0d177d5679b5af7e1a4b
BrajeshPatelGit <brajeshpatel@gmail.com>
2026-07-11 20:09:10 +0530

    updated values in code

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -1,33 +1,36 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "tag_name" {}
-variable "public_key" {}
+variable "key_name" {}
 variable "subnet_id" {}
-variable "sg_enable_ssh_https" {}
+variable "sg_for_jenkins" {}
 variable "enable_public_ip_address" {}
-variable "user_data_install_apache" {}
-variable "ec2_sg_name_for_python_api" {}
+variable "user_data_install_jenkins" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/ubuntu/keys/aws_ec2_terraform ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+  value = format("%s%s", "ssh -i /Users/rahulwagh/.ssh/keypair-vpc1 ubuntu@", aws_instance.jenkins_ec2_instance_ip.public_ip)
 }
 
-output "dev_proj_1_ec2_instance_id" {
-  value = aws_instance.dev_proj_1_ec2.id
+output "jenkins_ec2_instance_ip" {
+  value = aws_instance.jenkins_ec2_instance_ip.id
 }
 
-resource "aws_instance" "dev_proj_1_ec2" {
+output "dev_proj_1_ec2_instance_public_ip" {
+  value = aws_instance.jenkins_ec2_instance_ip.public_ip
+}
+
+resource "aws_instance" "jenkins_ec2_instance_ip" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = var.tag_name
   }
-  key_name                    = "aws_key"
+  key_name                    = "keypair-vpc1"
   subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
+  vpc_security_group_ids      = var.sg_for_jenkins
   associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_apache
+  user_data = var.user_data_install_jenkins
 
   metadata_options {
     http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
@@ -35,7 +38,7 @@ resource "aws_instance" "dev_proj_1_ec2" {
   }
 }
 
-resource "aws_key_pair" "dev_proj_1_public_key" {
-  key_name   = "aws_key"
-  public_key = var.public_key
+resource "aws_key_pair" "jenkins_ec2_instance_key_name" {
+  key_name   = "keypair-vpc1"
+  # key_name = var.key_name
 }
